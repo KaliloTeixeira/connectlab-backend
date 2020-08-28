56822763694e8ca97fb61b080c1dea6ee02dd051
KaliloTeixeira <kalilo555@hotmail.com>
2020-08-28 10:48:59 -0300

    fix/ Add description at school Model.

--- src/models/School.js
+++ src/models/School.js
@@ -29,6 +29,10 @@ const SchoolSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
+    description: {
+        type: String,
+        required: true,
+    },
     createdAt: {
         type: Date,
         default: Date.now,
